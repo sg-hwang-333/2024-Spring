e9aee6756c87d64264d3cd2837046c9981ec2a1e
sg-hwang-333 <hsg151515@naver.com>
2024-04-11 10:19:33 +0900

    [0411] github api 접근, RestTemplate를 이용한 외부 API 통신, ResponseEntity 활용

--- mvc/src/main/java/com/example/mvc/MyRenewController.java
+++ mvc/src/main/java/com/example/mvc/MyRenewController.java
@@ -1,12 +1,374 @@
 package com.example.mvc;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+class SignUpForm {
+    private String id;
+    private String email;
+    private String username;
+    private int age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "SignUpForm{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+// 커맨드 객체로 사용될 클래스 정의
+class MyCommandObject {
+
+    private String value1;
+    private Integer value2;
+
+    // 반드시 세터 메서드가 있어야 함
+    public void setValue1(String value1) { this.value1 = value1; }
+    public void setValue2(Integer value2) { this.value2 = value2; }
+
+    @Override
+    public String toString() {
+        return "MyCommandObject{" +
+                "value1='" + value1 + '\'' +
+                ", value2=" + value2 +
+                '}';
+    }
+}
+
+class MyJsonData {
+    private String value1;
+    private Integer value2;
+
+    public String getValue1() { return value1; }
+    public Integer getValue2() { return value2; }
+    public void setValue1(String value1) { this.value1 = value1; }
+    public void setValue2(Integer value2) { this.value2 = value2; }
+}
+
+// 내부 객체용 클래스 정의
+class InnerObject {
+    private String innerValue1;
+
+    // 세터, 게터 추가
+    public String getInnerValue1() {return innerValue1;}
+    public void setInnerValue1(String innerValue1) {this.innerValue1 = innerValue1;}
+}
+
+class JsonDataWithArrayAndInnerObject {
+    private List<String> array1; // String 형식의 데이터만 존재
+    private List<Object> array2; // 다양한 데이터 형식이 혼재
+    private InnerObject inner; // 내부 객체
+
+    // 세터, 게터 추가
+    public List<String> getArray1() {return array1;}
+    public void setArray1(List<String> array1) {this.array1 = array1;}
+    public List<Object> getArray2() {return array2;}
+    public void setArray2(List<Object> array2) {this.array2 = array2;}
+    public InnerObject getInner() {return inner;}
+    public void setInner(InnerObject inner) {this.inner = inner;}
+}
+
+class Test1 {
+    private String message;
+    private String from;
+    private String to;
+    private int importance;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getImportance() {
+        return importance;
+    }
+
+    public void setImportance(int importance) {
+        this.importance = importance;
+    }
+}
+
+class ResponseTest1 {
+    private String result;
+    private int code;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
+
+
+class ResponseTest2 {
+    private String result;
+    private String id;
+    private boolean success;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseTest2{" +
+                "result='" + result + '\'' +
+                ", id='" + id + '\'' +
+                ", success=" + success +
+                '}';
+    }
+}
+
+class RequestTest2 {
+    private String title;
+    private String author;
+    private int first_realese;
+    private double rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getFirst_realese() {
+        return first_realese;
+    }
+
+    public void setFirst_realese(int first_realese) {
+        this.first_realese = first_realese;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
+
+class RequestTest3 {
+    private String text;
+    private int count;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
+
+
 
 @RestController
 @RequestMapping("/renew")
 public class MyRenewController {
+
+    @GetMapping("/test1")
+    public ResponseTest1 responseTest1 (@RequestBody Test1 test1) {
+        System.out.println(test1.getMessage());
+        ResponseTest1 responseTest1 = new ResponseTest1();
+        responseTest1.setResult("success");
+        responseTest1.setCode(1001);
+
+        return responseTest1;
+    }
+
+    @GetMapping("/test2")
+    public ResponseTest2 responseTest2 (@RequestBody RequestTest2 requestTest2) {
+        System.out.println(requestTest2.toString());
+        ResponseTest2 responseTest2 = new ResponseTest2();
+        responseTest2.setId("a1234");
+        responseTest2.setResult("created");
+        responseTest2.setSuccess(true);
+
+        return responseTest2;
+    }
+
+    @GetMapping("/test3")
+    public String test(@RequestBody RequestTest3 requestTest3) {
+        String str = "";
+        for (int i = 0; i < requestTest3.getCount(); i++ ) {
+            str = str.concat(requestTest3.getText());
+        }
+        return str;
+    }
+
+
+
+    @PostMapping("/test1-2")
+    @ResponseBody
+    public HashMap<String, Object> test1_2(@RequestBody HashMap<String, Object> request) {
+        System.out.println(request);
+
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("result", "success");
+        response.put("code", 1001);
+
+        return response;
+    }
+    @PostMapping("/test2-2")
+    @ResponseBody
+    public HashMap<String, Object> test2_2(@RequestBody HashMap<String, Object> request) {
+        System.out.println(request);
+
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("result", "created");
+        response.put("id", "a1234");
+        response.put("success", true);
+
+        return response;
+    }
+    @PostMapping("/test3-2")
+    @ResponseBody
+    public String test3(@RequestBody HashMap<String, Object> request) {
+        String str = "";
+        for (int i = 0; i < parseInt(request.get("count").toString()); i++ ) {
+            str = str.concat((String) request.get("text"));
+        }
+        return str;
+    }
+
+
+    @GetMapping(value = "/json-test-2",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public JsonDataWithArrayAndInnerObject jsonTest2(@RequestBody JsonDataWithArrayAndInnerObject jsonDataWithArrayAndInnerObject) {
+        for(Object item : jsonDataWithArrayAndInnerObject.getArray2()) {
+            if(item != null) System.out.println(item.getClass().getName());
+        }
+        return jsonDataWithArrayAndInnerObject;
+    }
+
+
+
+    @PostMapping("/sign_up")
+    public String commandObjectTest(@ModelAttribute SignUpForm signUpForm) {
+        System.out.println(signUpForm);
+        return "success";
+    }
+
 
     // produces 옵션을 통해서 미디어 타입 지정 가능 (유추해서 자동으로 지정하게 할 수도 있지만 가급적 써주는 것을 권장)
     @GetMapping(value = "/echo", produces = MediaType.TEXT_PLAIN_VALUE)
@@ -25,5 +387,73 @@ public class MyRenewController {
         // Content-Type 헤더의 경우 produces 옵션을 제공하여 미디어 타입 지정 가능
         return bytesToString;
     }
+
+    // 또는
+    @GetMapping(value = "/echo2", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String echo2(@RequestBody String content) {
+        return content;
+    }
+
+    @GetMapping(value = "/hello-html",
+            produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String helloHTML() {
+        return "<h1>Hello</h1>";
+    }
+
+    @GetMapping(value = "/echo-repeat", produces = MediaType.TEXT_PLAIN_VALUE)
+// @RequestHeader 어노테이션을 통해서 X-Repeat-Count에 적힌 숫자 정보 가져오고 없으면 1로 초기화
+    public String echoRepeat(@RequestParam("word") String word, @RequestHeader(value = "X-Repeat-Count", defaultValue = "1") Integer repeatCount) throws IOException {
+        String result = "";
+        for (int i = 0; i < repeatCount; i++) {
+            result += word;
+        }
+        return result;
+    }
+
+
+    @GetMapping(value = "/dog-image", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] dogImage() throws IOException {
+        // resources 폴더의 static 폴더에 이미지 있어야 함
+        File file = ResourceUtils.getFile("classpath:static/Y9S1_GADGET_KEYART_1x1.jpg");
+        // 파일의 바이트 데이터 모두 읽어오기
+        byte[] bytes = Files.readAllBytes(file.toPath());
+
+        return bytes;
+    }
+
+    // 또는
+    @GetMapping(value = "/dog-image2", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] dogImage2() throws IOException {
+        File file = ResourceUtils.getFile("classpath:static/Y9S1_GADGET_KEYART_1x1.jpg");
+        return Files.readAllBytes(file.toPath());
+    }
+
+
+    @GetMapping(value = "/dog-image-file", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+// 헤더를 직접 조정하고 싶은 경우 ResponseEntity 타입을 반환하도록 설정 가능
+// (꺽쇠 안에는 응답 메시지의 바디 데이터에 포함될 타입을 지정)
+    public ResponseEntity<byte[]> dogImageFile() throws IOException {
+        File file = ResourceUtils.getFile("classpath:static/Y9S1_GADGET_KEYART_1x1.jpg");
+        byte[] bytes = Files.readAllBytes(file.toPath());
+
+        String filename = "Y9S1_GADGET_KEYART_1x1.jpg";
+        // 헤더 값 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=" + filename);
+
+        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/test")
+// @ModelAttribute 를 타입 앞에 붙여주고 메서드의 파라미터 값으로 전달되게 함
+    public String commandObjectTest(@ModelAttribute MyCommandObject myCommandObject) {
+        return myCommandObject.toString();
+    }
+
+
 
 }
---
+++ mvc/src/main/java/com/example/mvc/ResponseEntityDemo.java
@@ -0,0 +1,113 @@
+package com.example.mvc;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+class User {
+    private String id;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+}
+
+@RestController
+@RequestMapping("/response_entity_demo")
+public class ResponseEntityDemo {
+
+    @GetMapping("/demo1")
+    // 제네릭 타입이 Void인 이유는 해당 응답메시지에 바디데이터가 없기 때문임
+    public ResponseEntity<Void> demo1() {
+        // 상태 코드와 관련된 메서드와 build 메서드 호출하여 바로 바디 데이터 없이 응답
+        return ResponseEntity.ok().build();
+        // 바디데이터 없이 404 주고 싶은 경우
+        // return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/demo2")
+    // 제네릭 타입이 String이니 바디에 평문(text/plain)이 들어갈 것임을 유추 가능
+    public ResponseEntity<String> demo2() {
+        String responseBody = "Hello, world!";
+        // 상태 코드와 관련된 메소드에 바디 데이터를 같이 전달 가능
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @GetMapping("/demo3")
+    public ResponseEntity<String> demo3() {
+        // 좀 더 세분화해서 응답메시지를 커스터마이징하고 싶은 경우
+        // 헤더 영역 정보 만들기
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Custom-Header", "value");
+        String responseBody = "Hello, world!";
+        return ResponseEntity
+                .status(HttpStatus.OK) // 상태 코드
+                .headers(headers) // 헤더
+                .body(responseBody); // 바디
+    }
+
+    @GetMapping("/demo4")
+    public ResponseEntity<String> demo4() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Custom-Header", "value");
+        String responseBody = "Hello, world!";
+        // 빌더 패턴 없이 직접 new 키워드로 객체 생성하여 응답
+        return new ResponseEntity<>(
+                responseBody, // 바디
+                headers, // 헤더
+                HttpStatus.OK); // 상태 코드
+    }
+
+    @GetMapping("/demo5")
+    // 커스텀 클래스인 경우 내부적으로 JSON 직렬화 시도
+    // (아마도 JSON 포맷을 압도적으로 많이 쓰므로 그냥 해당 메시지 컨버터를 쓰도록 하는 듯?, 아마 객체 직렬화의 기본 방식이 JSON인듯함)
+    public ResponseEntity<User> demo5() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new User("1", "hello@example.com"));
+    }
+
+    @DeleteMapping("/demo6")
+    public ResponseEntity<Void> demo6() {
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/github/{user}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String githubUser(@PathVariable("user") String user) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 요청 메시지 생성 및 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        // 요청 메시지에 바디 데이터가 없으므로 Void 타입으로 설정
+        RequestEntity<Void> requestEntity = new RequestEntity<>(
+                null, headers, HttpMethod.GET, URI.create("https://api.github.com/users/" + user));
+
+        // 응답 메시지
+        ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
+        // 응답 메시지의 바디 데이터를 문자열로 해석
+        String responseBody = response.getBody();
+
+        return responseBody;
+    }
+}
